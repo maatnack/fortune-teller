fb79a10409fbd06780ed60ff37f359ae0fb163f3
maa <prince_ma@hotmail.com>
2017-04-08 00:53:32 +0200

    Uppdaterad FortuneTeller

--- src/main/java/se/nackademin/FortuneTeller.java
+++ src/main/java/se/nackademin/FortuneTeller.java
@@ -17,7 +17,161 @@ public class FortuneTeller {
      *
      */
     public String calculate() {
-        //TODO: Implement
+      
+        //TODO: Implement String strA = null; 
+      String strA = null;
+      String strB = null;
+      String strC = null;
+      String strD = null;
+      String strE = null;
+     int numA= magicNumbers.calculateA();
+        switch (numA) {
+            case 0:
+                strA=" stor";
+                break;
+            case 1:
+                strA=" liten";
+                break;
+            case 2:
+                strA="stark";
+                break;
+            case 3:
+                strA=" svag";
+                break;
+            case 4:
+                strA=" mjuk";
+                break;
+            case 5:
+                strA=" hård";
+                break;
+            case 6:
+                strA=" snabb";
+                break;
+            case 7:
+                strA=" vacker";
+                break;
+            case 8:
+                strA=" ljus";
+                break;
+            case 9:
+                strA=" mörk";
+                break;
+            default:
+                break;
+        }
+    
+            
+     int numB= magicNumbers.calculateB();
+     
+        switch (numB) {
+            case 0:
+                strB=" springa";
+                break;
+            case 1:
+                strB=" ljuga";
+                break;
+            case 2:
+                strB="flyga";
+                break;
+            case 3:
+                strB=" se";
+                break;
+            case 4:
+                strB=" vara";
+                break;
+            case 5:
+                strB=" äta";
+                break;
+            case 6:
+                strB=" mäta";
+                break;
+            case 7:
+                strB="  gå";
+                break;
+            case 8:
+                strB=" röra";
+                break;
+            case 9:
+                strB="resa";
+                break;
+            default:
+                break;
+        }
+    
+       
+     int numC= magicNumbers.calculateC();
+        switch (numC) {
+            case 0:
+                strC=" en lönehöjning";
+                break;
+            case 1:
+                strC="en lönesänkning ";
+                break;
+            case 2:
+                strC=" en fotboja";
+                break;
+            case 3:
+                strC=" en katt";
+                break;
+            case 4:
+                strC=" en hund";
+                break;
+            case 5:
+                strC="ett hus";
+                break;
+            case 6:
+                strC=" ett barn";
+                break;
+            case 7:
+                strC=" ett elstängsel";
+                break;
+            case 8:
+                strC=" en dator";
+                break;
+            case 9:
+                strC=" ett golv";
+                break;
+            default:
+                break;
+        }
+     
+     int numD= magicNumbers.calculateD();
+        switch (numD) {
+            case 0:
+                strD=" springa";
+                break;
+            case 1:
+                strD=" ljuga";
+                break;
+            case 2:
+                strD=" flyga";
+                break;
+            case 3:
+                strD=" se";
+                break;
+            case 4:
+                strD=" vara";
+                break;
+            case 5:
+                strD=" äta";
+                break;
+            case 6:
+                strD=" mäta";
+                break;
+            case 7:
+                strD=" gå";
+                break;
+            case 8:
+                strD=" röra";
+                break;
+            case 9:
+                strD=" resa";
+                break;
+            default:
+                break;
+        }
+                
+
         return "Not implemented!";
     }
 
